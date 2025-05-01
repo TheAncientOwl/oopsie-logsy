6c5af417d20d66e78a7ac35988f23326d123ddcc
TheAncientOwl <dele.alex.o.o@gmail.com>
2025-05-02 02:04:19 +0300

    Move double check to portal

--- src/components/ui/DoubleCheck.tsx
+++ src/components/ui/DoubleCheck.tsx
@@ -6,14 +6,14 @@
  *
  * @file DoubleCheck.tsx
  * @author Alexandru Delegeanu
- * @version 0.4
+ * @version 0.5
  * @description Self explanatory.
  */
 
 import React, { PropsWithChildren } from 'react';
 
 import { useColorModeValue } from '@/hooks/useColorMode';
-import { Box, BoxProps, Button, ButtonGroup, Stack } from '@chakra-ui/react';
+import { Box, BoxProps, Button, ButtonGroup, Portal, Stack } from '@chakra-ui/react';
 
 type TDoubleCheckProps = {
   isShown: boolean;
@@ -29,52 +29,54 @@ export const DoubleCheck: React.FC<TDoubleCheckProps & PropsWithChildren> = prop
   const backgroundColor = useColorModeValue('gray.500', 'gray.800');
 
   return (
-    <Stack
-      display={props.isShown ? 'flex' : 'none'}
-      visibility={props.isShown ? 'visible' : 'hidden'}
-      position='fixed'
-      zIndex='1000000'
-      top='0'
-      bottom='0'
-      left='0'
-      right='0'
-      justifyContent='center'
-      alignItems='center'
-      backgroundColor='rgba(0, 0, 0, 0.5)'
-      overflow='hidden'
-    >
+    <Portal>
       <Stack
+        display={props.isShown ? 'flex' : 'none'}
+        visibility={props.isShown ? 'visible' : 'hidden'}
+        position='fixed'
+        zIndex='1000000'
+        top='0'
+        bottom='0'
+        left='0'
+        right='0'
         justifyContent='center'
         alignItems='center'
-        backgroundColor={backgroundColor}
-        boxShadow='md'
-        borderRadius='10px'
-        padding='2rem 5rem'
+        backgroundColor='rgba(0, 0, 0, 0.5)'
+        overflow='hidden'
       >
-        <Box fontWeight='bold' fontSize='1.2em' mb='1em'>
-          {props.label}
-        </Box>
-
-        <Box mb='1.5em' textAlign='center' {...props.contentProps}>
-          {props.children}
-        </Box>
-
-        <ButtonGroup
-          size='md'
-          variant='surface'
-          display='flex'
-          gap='10rem'
+        <Stack
           justifyContent='center'
-          width='100%'
+          alignItems='center'
+          backgroundColor={backgroundColor}
+          boxShadow='md'
+          borderRadius='10px'
+          padding='2rem 5rem'
         >
-          <Button colorPalette='green' onClick={props.onAccept}>
-            {props.acceptLabel}
-          </Button>
-          <Button colorPalette='red' onClick={props.onDecline}>
-            {props.declineLabel}
-          </Button>
-        </ButtonGroup>
+          <Box fontWeight='bold' fontSize='1.2em' mb='1em'>
+            {props.label}
+          </Box>
+
+          <Box mb='1.5em' textAlign='center' {...props.contentProps}>
+            {props.children}
+          </Box>
+
+          <ButtonGroup
+            size='md'
+            variant='surface'
+            display='flex'
+            gap='10rem'
+            justifyContent='center'
+            width='100%'
+          >
+            <Button colorPalette='green' onClick={props.onAccept}>
+              {props.acceptLabel}
+            </Button>
+            <Button colorPalette='red' onClick={props.onDecline}>
+              {props.declineLabel}
+            </Button>
+          </ButtonGroup>
+        </Stack>
       </Stack>
-    </Stack>
+    </Portal>
   );
 };
